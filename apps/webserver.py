38edd39e7e1d03716c08038d127756d3209614f2
Sami Pellinen <sami@pell.fi>
2026-05-02 04:19:21 +0000

    Webserver

---
+++ apps/webserver.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hello, Polymarket Trader! 🤘"
+
+@app.get("/health")
+@app.get("/healthcheck")
+async def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
